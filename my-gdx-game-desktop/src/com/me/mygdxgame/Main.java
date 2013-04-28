9e0ed259d59f7a0a901cc2767045351237978e2d
drinor <drinor2@gmail.com>
2013-04-28 16:35:35 +0200

    Desktop project.

---
+++ my-gdx-game-desktop/src/com/me/mygdxgame/Main.java
@@ -0,0 +1,17 @@
+package com.me.mygdxgame;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdxgame.MyGdxGame;
+
+public class Main {
+	public static void main(String[] args) {
+		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+		cfg.title = "my-gdx-game";
+		cfg.useGL20 = false;
+		cfg.width = 480;
+		cfg.height = 320;
+		
+		new LwjglApplication(new MyGdxGame(), cfg);
+	}
+}
